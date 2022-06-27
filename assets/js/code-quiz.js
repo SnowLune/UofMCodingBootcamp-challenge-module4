8839f8df407261fbe5b414cc9a10fc09f32010af
Luna R <snowluna@protonmail.com>
2022-06-27 13:02:46 +0000

    create questions class and add questions, update js/html

--- assets/js/code-quiz.js
+++ assets/js/code-quiz.js
@@ -1,5 +1,14 @@
+// state vars
+var currentQuestionIndex = 0;
+var timerId;
+var timeLimit = questions.length * 10;
+
 // DOM element variables
 var startBtn = document.getElementById("start-button");
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit-question");
 
 // Start Quiz function
 function startQuiz() {
@@ -13,7 +22,14 @@ function startQuiz() {
    timerId = setInterval(clockTick, 1000);
    timerEl.textContent = time;
 
-   addQuestion();
+   getQuestion();
+}
+
+function getQuestion() {
+   var currentQuestion = questions[currentQuestionIndex];
+
+   var textEl = document.getElementById("question-text");
+   textEl.textContent = currentQuestion.text;
 }
 
 // Start Quiz listener
---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+class Question {
+   constructor(t, answers, correctAnswerIndex) {
+      this.text = t;
+      this.answers = answers;
+      this.correctAnswer = answers[correctAnswerIndex];
+   }
+}
+
+questions = [
+   new Question(
+      "Which of the following is NOT a basic data type?",
+      ["numbers", "date", "null", "boolean"],
+      1
+   ),
+
+   new Question(
+      "JavaScript is a...",
+      ["compiled language", "interpreted language"],
+      1
+   ),
+   new Question(
+      "JavaScript is a strongly typed language.",
+      ["true", "false"],
+      1
+   ),
+   new Question("JavaScript only runs in browsers.", ["true", "false"], 0)
+];
